74a0c11116f881eb7119e4e731c4b37b7c2456ae
sara altaee <sara.korea96@gmail.com>
2020-05-18 20:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,15 @@
 let dodger = document.getElementById("dodger");
 
 function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
- 
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
-  }
+let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+    if (left<360) {
+      dodger.style.left =`${left+1}px`;
+    }
 }
-
-
-document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowRight"){
+    moveDodgerRight();
   }
 });
 
