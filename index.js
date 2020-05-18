6985dd1b8e85265deb8d3aca9a7d227a12a9ad12
sara altaee <sara.korea96@gmail.com>
2020-05-18 20:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerLeft(){
 function moveDodgerRight(){
     let leftNumbers = dodger.style.left.replace("px","");
     let left = parseInt(leftNumbers, 10);
-    if (left<360) {
+    if (left<0) {
       dodger.style.left =`${left+1}px`;
     }
 }
