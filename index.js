764c413a0f57e71280157f055ae240d9a5f223f9
sara altaee <sara.korea96@gmail.com>
2020-05-18 20:40:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// Your code 
+let dodger = document.getElementById('dodger');
+let game = document.getElementById('game');
+
+
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+    if (left>0) {
+      dodger.style.left =`${left - 1}px`;
+    }
+}
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+    if (left<180) {
+      dodger.style.left =`${left+1}px`;
+    }
+}
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+})
+
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowLeft"){
+    moveDodgerLeft();
+  }
+})
