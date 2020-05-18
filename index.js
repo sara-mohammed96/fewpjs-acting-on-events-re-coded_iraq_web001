de941feb378a4a640429bedec9ace77d2f5b9d5b
sara altaee <sara.korea96@gmail.com>
2020-05-18 20:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code 
+let dodger = document.getElementById('dodger');
+let game = document.getElementById('game');
+dodger.style.backgroundColor = "#FF69B4";
+console.log(dodger.style.left);
+console.log(dodger.style.bottom);
+dodger.style.bottom = "0px";
+function moveDodgerRight(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+    if (left<360) {
+      dodger.style.left =`${left+1}px`;
+    }
+}
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+})
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px","");
+    let left = parseInt(leftNumbers, 10);
+    if (left>0) {
+      dodger.style.left =`${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowLeft"){
+    moveDodgerLeft();
+  }
+})
